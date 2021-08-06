005b515467364af4dccbd02481f344ad08b85b42
Lariod <nguyenhuythinh7@gmail.com>
2021-08-06 23:52:24 +0700

    new add scene battle loading

---
+++ Assets/LoaderBattle.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoaderBattle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterSystem.GetProfile.ImageLoading 
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
